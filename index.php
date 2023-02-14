f09270e6dff7ab3b52322586ea65eab8948268fd
Abrohoussein <75318057+Abrohoussein@users.noreply.github.com>
2023-02-14 16:46:12 +0100

    TEST IMAGE

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
-<h1>SITE APRES MODIFICATION LE 14/02/2022</h1>
+<?php
+echo '<div align="center"><font face="arial" size="2" color="blue"> Bonjour le monde !</font><br /> ';
+echo '<img src="images/globe.gif" border="0" /></div> ';
+?>
