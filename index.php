0c4271a7767bd34aec525881ba1dc732406b9ecf
Abrohoussein <75318057+Abrohoussein@users.noreply.github.com>
2023-02-14 17:20:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,10 @@
 echo '<div align="center"><font face="arial" size="5" color="grey"> Bonjour le monde !</font><br /> ';
 echo '<img src="el.png" border="0" /></div> ';
 $ressource = fopen('data.txt', 'rb');
-echo fread($ressource, filesize('data.txt'));
+$_PUT = array();
+parse_str(file_get_contents($ressource), $_PUT);
+foreach ($_PUT as $key => $value)
+  {
+    echo $key . " : " . $value;
+  }
 ?>
