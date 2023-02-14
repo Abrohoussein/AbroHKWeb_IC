fa9892b7a36e639051c23d8fc84efb81f3326563
Abrohoussein <75318057+Abrohoussein@users.noreply.github.com>
2023-02-14 16:57:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo '<div align="center"><font face="arial" size="5" color="green"> Bonjour le monde !</font><br /> ';
-echo '<img src="images/el.png" border="0" /></div> ';
+echo '<div align="center"><font face="arial" size="5" color="grey"> Bonjour le monde !</font><br /> ';
+echo '<img src="el.png" border="0" /></div> ';
 ?>
