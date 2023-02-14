f28cb404c182557997bd5652a51ac92cb9673e28
Abrohoussein <75318057+Abrohoussein@users.noreply.github.com>
2023-02-14 16:50:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo '<div align="center"><font face="arial" size="2" color="grey"> Bonjour le monde !</font><br /> ';
+echo '<div align="center"><font face="arial" size="5" color="grey"> Bonjour le monde !</font><br /> ';
 echo '<img src="images/elephant.jpg" border="0" /></div> ';
 ?>
