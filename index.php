ea7733f7299074a5f48706f3d9d96e3032634017
Abrohoussein <75318057+Abrohoussein@users.noreply.github.com>
2023-02-17 09:45:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 echo '<div align="center"><font face="arial" size="5" color="grey"> Bonjour le monde !</font><br /> ';
 echo '<img src="el.png" border="0" /></div> ';
-$ressource = fopen('data.txt', 'rb');
-echo fread($ressource, filesize('test.txt'));
+<body>
+        <h1>Titre principal</h1>
+        <?php       
+            $ressource = fopen('data.txt', 'rb');
+            echo fread($ressource, filesize('data.txt'));
+        ?>
+        <p>Un paragraphe</p>
+    </body>
 ?>
