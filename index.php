bff990a18d05299291841a2643899733b55add69
Abrohoussein <75318057+Abrohoussein@users.noreply.github.com>
2023-02-17 09:24:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo '<div align="center"><font face="arial" size="5" color="grey"> Bonjour le monde !</font><br /> ';
-echo '<img src="nyan-cat-gif-1.gif" border="0" /></div> ';
+echo '<img src="el.png" border="0" /></div> ';
 $ressource = fopen('data.txt', 'rb');
 $_PUT = array();
 parse_str(file_get_contents("http://localhost/dolibarr/api/index.php/products?DOLAPIKEY=abr99&sortfield=t.ref&sortorder=ASC&limit=100"), $_PUT);
