7463408f9286633514ea0afb44fb740e4d2e9196
Abrohoussein <75318057+Abrohoussein@users.noreply.github.com>
2023-07-09 14:22:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php       
-     $ressource = fopen('data.txt', 'rb');
-     echo fread($ressource, filesize('data.txt'));        
-     foreach ($ressource as $value){
-         print($value);
-     }
+     print("l'aapp est modifier poto");
 ?>
