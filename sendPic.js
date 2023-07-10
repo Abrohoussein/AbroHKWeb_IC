6b30dc45cfeb71b79adf1f3ecad1415742cfc9d7
Abrohoussein <75318057+Abrohoussein@users.noreply.github.com>
2023-07-10 13:22:19 +0200

    Update sendPic.js

--- sendPic.js
+++ sendPic.js
@@ -4,9 +4,15 @@ const video = document.getElementById('video');
 // Accéder au bouton de capture de photo
 const captureBtn = document.getElementById('capture-btn');
 
+// Accéder au bouton d'analyse de photo
+const processBtn = document.getElementById('process-btn');
+
 // Accéder au canvas pour afficher la photo capturée
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
+
+// Variable pour stocker la photo capturée
+let capturedPhoto = null;
 
 // Obtenir l'accès à la caméra
 navigator.mediaDevices.getUserMedia({ video: true })
@@ -26,9 +32,59 @@ function capturePhoto() {
   // Obtenir la photo sous forme de base64
   const photoData = canvas.toDataURL('image/jpeg');
   
-  // Envoyer la photo au serveur pour traitement (par exemple, en utilisant une requête AJAX)
-  // ...
+  // Stocker la photo capturée dans la variable
+  capturedPhoto = photoData;
+  
+  // Afficher la photo capturée (optionnel)
+  const img = document.createElement('img');
+  img.src = photoData;
+  document.body.appendChild(img);
 }
 
-// Ajouter un gestionnaire d'événement au bouton de capture de photo
+// Fonction pour analyser la photo
+function processPhoto() {
+  // Vérifier si une photo a été capturée
+  if (capturedPhoto === null) {
+    console.log('Aucune photo capturée.');
+    return;
+  }
+  
+  // Construction de la commande pour exécuter le script Python avec la photo en entrée
+  const command = `python main.py ${capturedPhoto}`;
+  
+  // Exécution du script Python avec la photo en entrée
+  const { exec } = require('child_process');
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Erreur d'exécution du script Python : ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Erreur de sortie du script Python : ${stderr}`);
+      return;
+    }
+    
+    // Récupérer le résultat de la prédiction depuis stdout (à adapter en fonction de la sortie du script Python)
+    const predictionResult = stdout;
+    
+    // Créer un objet contenant le résultat de la prédiction
+    const predictionData = {
+      prediction: predictionResult
+    };
+    
+    // Convertir l'objet en chaîne JSON
+    const predictionJSON = JSON.stringify(predictionData);
+    
+    // Créer un lien de téléchargement pour le fichier JSON
+    const blob = new Blob([predictionJSON], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'prediction.json';
+    link.click();
+  });
+}
+
+// Ajouter des écouteurs d'événement aux boutons
 captureBtn.addEventListener('click', capturePhoto);
+processBtn.addEventListener('click', processPhoto);
