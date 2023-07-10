b73a5cb202173402df9bbff936ff98b5aed05fa6
Abrohoussein <75318057+Abrohoussein@users.noreply.github.com>
2023-07-10 18:54:32 +0200

    Create save-photo.php

---
+++ save-photo.php
@@ -0,0 +1,15 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $photoData = $_POST['photoData'];
+  
+  // Chemin du fichier de destination
+  $filePath = 'capture.jpg';
+
+  // Enregistrer la photo sur le serveur
+  if (file_put_contents($filePath, base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $photoData)))) {
+    echo 'La photo a été enregistrée avec succès sur le serveur.';
+  } else {
+    echo 'Erreur lors de l\'enregistrement de la photo sur le serveur.';
+  }
+}
+?>
