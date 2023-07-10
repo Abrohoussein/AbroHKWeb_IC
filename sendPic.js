04ec5cd785373fcd296c2dd64a8669f985cf43fc
Abrohoussein <75318057+Abrohoussein@users.noreply.github.com>
2023-07-10 15:15:05 +0200

    Update sendPic.js

--- sendPic.js
+++ sendPic.js
@@ -45,17 +45,6 @@ function capturePhoto() {
   };
   img.src = photoData;
 
-  // Obtenir la photo sous forme de base64
-  const photoSave = canvas.toDataURL('image/jpeg').replace(/^data:image\/jpeg;base64,/, '');
-
-  // Écrire les données de la photo dans le fichier "photo.jpg"
-  fs.writeFile('photo.jpg', photoSave, 'base64', function(err) {
-    if (err) {
-      console.log('Erreur lors de l\'écriture du fichier :', err);
-    } else {
-      console.log('Photo capturée et enregistrée avec succès dans "photo.jpg"');
-    }
-  });
 }
 
 // Fonction pour analyser la photo
