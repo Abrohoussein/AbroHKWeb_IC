91fe5139862dd5209e2047abc7d7ddbd96a70e60
Abrohoussein <75318057+Abrohoussein@users.noreply.github.com>
2023-07-11 02:20:33 +0200

    Update save-photo.php

--- save-photo.php
+++ save-photo.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $photoData = $_POST['photoData'];
   
   // Chemin du fichier de destination
-  $filePath = 'capture.jpg';
+  $filePath = 'capturetmp.jpg';
 
   // Enregistrer la photo sur le serveur
   if (file_put_contents($filePath, base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $photoData)))) {
